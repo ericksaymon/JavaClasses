c36f2676e822f6903ede1b1adab125d568fc5f3d
Erick Saymon <erickcontadini@gmail.com>
2024-09-02 15:21:20 -0300

    New Activities

--- src/cloud/ericksaymon/maratonajava/javacore/Bmethodsintroduction/domain/Employee.java
+++ src/cloud/ericksaymon/maratonajava/javacore/Bmethodsintroduction/domain/Employee.java
@@ -1,9 +1,34 @@
 package cloud.ericksaymon.maratonajava.javacore.Bmethodsintroduction.domain;
 
 public class Employee {
-    public String name;
-    public int age;
-    public double[] salary;
+    private String name;
+    private int age;
+    private double[] salary;
+    private double sum;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double[] salary) {
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double[] getSalary() {
+        return this.salary;
+    }
 
     public void printData() {
         System.out.println(this.name);
@@ -14,7 +39,6 @@ public class Employee {
     }
 
     public void averageSalary() {
-        double sum = 0;
         for (double temp: this.salary) {
             sum += temp;
         }
--- src/cloud/ericksaymon/maratonajava/javacore/Bmethodsintroduction/test/TestEmployee.java
+++ src/cloud/ericksaymon/maratonajava/javacore/Bmethodsintroduction/test/TestEmployee.java
@@ -6,9 +6,9 @@ public class TestEmployee {
     public static void main(String[] args) {
         Employee employee = new Employee();
 
-        employee.name = "Theo";
-        employee.age = 22;
-        employee.salary = new double[] {2000D, 1900D, 2000D};
+        employee.setName("Theo");
+        employee.setAge(22);
+        employee.setSalary(new double[] {2000D, 1900D, 2000D});
 
         employee.printData();
         employee.averageSalary();
